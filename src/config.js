c230091d9425d3636956812d51bd40dc4874ca66
yuta ikoma <ikoma.tec@gmail.com>
2026-01-03 06:03:06 +0900

    fix: 環境変数の設定

--- src/config.js
+++ src/config.js
@@ -4,10 +4,11 @@ export const API_BASE_URL =
 
 // デモ用静的トークン: 本番では必ず安全に管理してください
 export const API_TOKEN =
-  import.meta.env.VITE_API_TOKEN || 'cc94b69e011cf9128c98f3c90946bd8e1a6f022ebb6fdbf65f50a81cb3152946';
+  import.meta.env.VITE_API_TOKEN || 'VITE_API_TOKEN';
 
 // 記事のコンテンツ定義ID。必要に応じて変更してください
 export const TOPICS_GROUP_ID = 1;
 
 export const PAGE_SIZE = 12;
+
 
